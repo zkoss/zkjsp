ba980dc399d349ff102894a1a5067428f97f133d
benbai123 <benbai123@76961352-46ec-fe10-6487-13f8d6e24fef>
2011-07-20 11:23:50 +0000

    move zkjsp to google code

---
+++ tool/src/tool/ComponentMap.java
@@ -0,0 +1,111 @@
+/* ComponentMap.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Nov 21, 2008 4:21:28 PM 2007, Created by Flyworld
+}}IS_NOTE
+
+Copyright (C) 2008 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under GPL Version 2.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+ */
+package tool;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.zkoss.zk.ui.metainfo.ComponentDefinition;
+import org.zkoss.zk.ui.metainfo.ComponentDefinitionMap;
+import org.zkoss.zk.ui.metainfo.LanguageDefinition;
+/**
+ * 
+ * @author Ian Y.T Tsai(zanyking)
+ *
+ */
+public class ComponentMap {
+	Map<String, ArrayList> varMap;
+	int _size = -1;
+
+	public ComponentMap() {
+		varMap = new HashMap<String, ArrayList>();
+	}
+
+	public void setLanguageDefinition(LanguageDefinition ld) {
+		ComponentDefinitionMap map = ld.getComponentDefinitionMap();
+		ArrayList al = new ArrayList<String>();
+		al.addAll(map.getNames());
+		ComponentMap cmpMap = new ComponentMap();
+		_size = al.size();
+		for (int i = 0; i < al.size(); i++) {
+			ComponentDefinition cmpdefi = ld.getComponentDefinition(al.get(i)
+					.toString());
+			Class cls = (Class) cmpdefi.getImplementationClass();
+			this.addValue(cls.getPackage().getName(), al.get(i).toString());
+		}
+	}
+
+	private void addValue(String packageName, String className) {
+		if (varMap.containsKey(packageName)) {
+			ArrayList tempArrayList = varMap.get(packageName);
+			varMap.remove(packageName);
+			tempArrayList.add(className);
+			varMap.put(packageName, tempArrayList);
+		} else {
+			ArrayList tempArrayList = new ArrayList();
+			tempArrayList.add(className);
+			varMap.put(packageName, tempArrayList);
+		}
+	}
+
+	public int getMapSize() {
+		return _size;
+	}
+
+	public Object[] getMapKeysObj() {
+		return getMapKeys();
+	}
+
+	public Object[] getMapValuesObj() {
+		return varMap.values().toArray();
+	}
+
+	/**
+	 * get sorted map keys
+	 * */
+	public String[] getMapKeys() {
+		String[] allKeys = (String[]) varMap.keySet().toArray(new String[] {});
+		ArrayList alst = new ArrayList();
+		for (String key : allKeys) {
+			alst.add(key);
+		}
+		Collections.sort(alst);
+		return (String[]) alst.toArray(new String[] {});
+	}
+
+	/**
+	 * get sorted map values with sorted keys
+	 * */
+	public Object[][] getMapVales() {
+		String[] allKeys = getMapKeys();
+		List<String[]> lst = new LinkedList<String[]>();
+
+		for (String key : allKeys) {
+			ArrayList al = varMap.get(key);
+			Collections.sort(al);
+			String[] temp = (String[]) al.toArray(new String[] {});
+			lst.add(temp);
+		}
+		return lst.toArray(new String[][] {});
+
+	}
+}
---
+++ zkjsp-demo/WebContent/zkspreadsheet.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://www.zkoss.org/jsp/zul" prefix="z" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>ZK Spreadsheet</title>
+</head>
+<body>
+<z:page zscriptLanguage="java"><%-- this is Jsp valid Comment, do not use XML comment. --%>
+	<h2>ZK Spreadsheet</h2>
+	<z:window height="100%">
+		<z:spreadsheet id="fluSpreadsheet" src="/xls/swineFlu.xls" width="100%" height="100%" maxrows="51" maxcolumns="4" style="border:1px solid black;"/>
+	</z:window>
+</z:page>
+</body>
+</html>
